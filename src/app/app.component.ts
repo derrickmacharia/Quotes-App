734ef9418ca9d6e2ab37792619d73bf5e990b08a
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-10-09 12:00:19 +0300

    Initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes';
+  quotes:string[];
+
+  constructor(){
+    this.quotes =['Ezekiel Kibyego','Derrick Macharia','Diana Mongina']
+  }
 }
